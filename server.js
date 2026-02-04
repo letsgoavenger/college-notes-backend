aea6d99809cbf3621584c3a3c50aefb3ba662ea8
Vp Kamboj <letsgoavenger@gmail.com>
2026-02-04 15:57:50 +0530

    final railway build fix

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// test route (TOP pe hi)
+// test route
 app.get("/test", (req, res) => {
   res.send("TEST OK");
 });
 
-// home route
+// home
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
 // static uploads
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // routes
 app.use("/admin", require("./routes/admin"));
 app.use("/files", require("./routes/files"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
